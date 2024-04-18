d0cc044d805d5887c46d3608857ff389c61ff527
Alvin Leung <alvinleung2009@gmail.com>
2024-04-17 21:09:33 -0400

    landing section

---
+++ src/components/MainNav/MainNav.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Logo from "../Logo/Logo";
+
+type Props = {};
+
+const MainNav = (props: Props) => {
+  return (
+    <div className="grid grid-cols-8 h-[80vh] mx-4 my-4">
+      <h1 className="font-brand-md max-w-[30ch] col-span-5">
+        Daybreak Studio combines technology with design to shape brand, web, and
+        software experiences.
+      </h1>
+      <div className="col-span-2 flex flex-col gap-2">
+        <a className="font-cond-xs opacity-50">hello@daybreak.studio</a>
+        <a className="font-cond-xs opacity-50">careers@daybreak.studio</a>
+      </div>
+      <div className="col-span-1 flex flex-col gap-2">
+        <a className="font-cond-xs opacity-50">Instagram</a>
+        <a className="font-cond-xs opacity-50">Twitter</a>
+      </div>
+
+      <div className="col-span-5 flex flex-row items-end justify-start">
+        <Logo />
+      </div>
+      <nav className="col-span-2 flex flex-row gap-6 items-end mb-4">
+        <a className="font-cond-xs opacity-50">Home</a>
+        <a className="font-cond-xs opacity-50">About</a>
+        <a className="font-cond-xs opacity-50">Team</a>
+        <a className="font-cond-xs opacity-50">Contact</a>
+      </nav>
+      <div className="col-span-1 flex flex-row gap-6 mb-4 items-end font-cond-xs opacity-50 ">
+        Stay up to date
+      </div>
+    </div>
+  );
+};
+
+export default MainNav;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import TypeSystemPlugin from "./TypeSystemPlugin";
 
 const config: Config = {
   content: [
@@ -15,6 +16,64 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    TypeSystemPlugin({
+      // default base stlying (mobile)
+      default: {
+        // founders grotesk
+        "sans-sm": {
+          "font-family": "var(--font-founders-grotesk-regular)",
+          "font-size": "16px",
+          "letter-spacing": "0.0rem",
+          "line-height": "20px",
+        },
+        "sans-xs": {
+          "font-family": "var(--font-founders-grotesk-regular)",
+          "font-size": "14px",
+          "letter-spacing": "0.0rem",
+          "line-height": "20px",
+        },
+        // founders grotesk condensed
+        "cond-xs": {
+          "font-family": "var(--font-founders-grotesk-condensed-regular)",
+          "font-size": "14px",
+          "letter-spacing": "0.02rem",
+          "line-height": "85%",
+          "text-transform": "uppercase",
+        },
+        "cond-sm": {
+          "font-family": "var(--font-founders-grotesk-condensed-regular)",
+          "font-size": "16px",
+          "letter-spacing": "0.02rem",
+          "line-height": "85%",
+          "text-transform": "uppercase",
+        },
+        // factul
+        "brand-xl": {
+          "font-family": "var(--font-fractul-regular)",
+          "font-size": "72px",
+          "letter-spacing": "-0.03rem",
+          "line-height": "64px",
+        },
+        "brand-lg": {
+          "font-family": "var(--font-fractul-regular)",
+          "font-size": "64px",
+          "letter-spacing": "-0.03rem",
+          "line-height": "64px",
+        },
+        "brand-md": {
+          "font-family": "var(--font-fractul-regular)",
+          "font-size": "24px",
+          "letter-spacing": "-0.02rem",
+          "line-height": "32px",
+        },
+      },
+
+      // When screen is at lg breakpoint (desktop)
+      lg: {
+        // mono
+      },
+    }),
+  ],
 };
 export default config;
